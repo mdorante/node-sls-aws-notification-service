0a06b60d0783ed84fb06569d5c4fc08c2287332a
mdorante <mdorante10@gmail.com>
2020-08-22 23:33:49 +0000

    update sendMail function (it actually sends mails now)

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -1,6 +1,33 @@
+import AWS from "aws-sdk";
+
+const ses = new AWS.SES({ region: process.env.REGION });
+const sesEmailAddress = process.env.EMAIL;
+
 async function sendMail(event, context) {
-  console.log(event);
-  return event;
+  const params = {
+    Source: sesEmailAddress,
+    Destination: {
+      ToAddresses: ["mdorante10@gmail.com"],
+    },
+    Message: {
+      Subject: {
+        Data: "Test SES",
+      },
+      Body: {
+        Text: {
+          Data: "Hello from SES!",
+        },
+      },
+    },
+  };
+
+  try {
+    const result = await ses.sendEmail(params).promise();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export const handler = sendMail;
